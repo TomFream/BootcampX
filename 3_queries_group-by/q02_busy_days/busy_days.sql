22f8fc76855466aa3625a54f80558889fb505a30
Tom Fream <tomfream@gmail.com>
2021-04-26 19:34:17 -0600

    BootcampX queries 3 - Group By

---
+++ 3_queries_group-by/q02_busy_days/busy_days.sql
@@ -0,0 +1,11 @@
+SELECT day, count(*) as total_assignments
+FROM assignments
+GROUP BY day
+HAVING count(*) >= 10
+ORDER BY day;
+
+-- SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+-- FROM assignment_submissions
+-- JOIN students ON students.id = student_id
+-- WHERE students.end_date IS NULL
+-- GROUP BY students.name;
---
+++ 3_queries_group-by/q03_large_cohorts/large_cohorts.sql
@@ -0,0 +1,8 @@
+SELECT cohorts.name as cohort_name, count(students.cohort_id) as student_count
+FROM cohorts
+JOIN students ON students.cohort_id = cohorts.id
+GROUP BY cohort_name
+HAVING count(students.cohort_id) >= 18
+ORDER BY student_count;
+
+
