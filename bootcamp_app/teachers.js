24a5cc30095ace7c1c7383a052a903eb2e973f4d
Tom Fream <tomfream@gmail.com>
2021-04-28 23:36:48 -0600

    Refactors node postgres to prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,15 +9,20 @@ const pool = new Pool({
 
 const args = process.argv.slice(2);
 
-pool.query(`
+const queryString = `
   SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
   FROM teachers
   JOIN assistance_requests ON teacher_id = teachers.id
   JOIN students ON student_id = students.id
   JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name = '${args[0] || 'JUL02'}'
+  WHERE cohorts.name LIKE $1
   ORDER BY teacher;
-`).then(res => {
+`;
+
+const cohortName = args[0];
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values).then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
   })
